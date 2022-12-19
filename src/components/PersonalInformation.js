2e4d268ca123b2c10b540fd2bd51ac09971f2980
Joel Singh <joelsingh788@gmail.com>
2022-12-19 10:46:47 -0500

    Style PersonalInformation with card class

--- src/components/PersonalInformation.js
+++ src/components/PersonalInformation.js
@@ -7,7 +7,7 @@ export default class PersonalInformation extends Component {
 
   render() {
     return (
-      <div>
+      <div className="card">
         <input placeholder="First Name" type="text" />
         <input placeholder="Last Name" type="text" />
         <input placeholder="Title" type="text" />
