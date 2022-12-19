b99da7ab9f3b9bd49ba3f0a659eb7e31f9ced93d
Joel Singh <joelsingh788@gmail.com>
2022-12-19 10:24:28 -0500

    Add gray background with css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PersonalInformation from "./components/PersonalInformation";
+import './style.css'
 
 export default class App extends Component {
   constructor(props) {
@@ -7,7 +8,7 @@ export default class App extends Component {
   }
 
   render() {
-    return (<div>
+    return (<div className="app">
         <PersonalInformation />
       </div>);
   }
