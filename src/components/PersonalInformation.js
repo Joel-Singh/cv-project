6694e8c5d7e468fb5261041ae34b4a6b42e9f041
Joel Singh <joelsingh788@gmail.com>
2022-12-19 09:44:55 -0500

    Add and rearrange inputs in PersonalInformation

--- src/components/PersonalInformation.js
+++ src/components/PersonalInformation.js
@@ -8,9 +8,13 @@ export default class PersonalInformation extends Component {
   render() {
     return (
       <div>
-        <input placeholder="Name" type="text" />
-        <input placeholder="Email" type="text" />
+        <input placeholder="First Name" type="text" />
+        <input placeholder="Last Name" type="text" />
+        <input placeholder="Title" type="text" />
+        <input placeholder="Address" type="text" />
         <input placeholder="Phone Number" type="number" />
+        <input placeholder="Email" type="text" />
+        <input placeholder="Description" type="text" />
       </div>
     )
   }
